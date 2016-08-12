7f7328eab5f6a1cb2d803ccbc0e9e0a5f861c852
Carsten Hartenfels <hartenfels@uni-koblenz.de>
2016-08-12 20:39:20 +0200

    Add Alt-Super-i shortcut for firefox

--- config.h
+++ config.h
@@ -61,11 +61,13 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "terminator", NULL };
+static const char *firefoxcmd[] = { "firefox-bin", NULL };
 
 static Key keys[] = {
 	/* applications are run with Alt-Super-KEY */
 	/*     key   command                       */
 	APPKEY(XK_t, termcmd)
+	APPKEY(XK_t, firefoxcmd)
 
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
