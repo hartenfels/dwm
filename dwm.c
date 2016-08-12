da72b8cdf195ce35fce047424ed06677311f561c
Carsten Hartenfels <hartenfels@uni-koblenz.de>
2016-08-12 19:59:57 +0200

    Start with tiling layout, not floating

--- dwm.c
+++ dwm.c
@@ -758,7 +758,7 @@ createmon(void)
 	for (i = 0; i < WORKSPACES; ++i) {
 		Workspace *ws = &m->workspaces[i];
 		ws->mfact = mfact;
-		ws->layout = 0;
+		ws->layout = &layouts[0];
 		ws->label[0] = '\0';
 	}
 
