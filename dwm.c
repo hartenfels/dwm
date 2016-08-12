d2381d66f47a973a9fab6dd837a0dc2e61b5bae4
Carsten Hartenfels <hartenfels@uni-koblenz.de>
2016-08-12 20:08:53 +0200

    Don't arrange twice when switching workspaces

--- dwm.c
+++ dwm.c
@@ -2404,7 +2404,6 @@ view(const Arg *arg)
 	if (arg->ui & TAGMASK)
 		selmon->tagset[selmon->seltags] = arg->ui & TAGMASK;
 	focus(NULL);
-	arrange(selmon);
 
 	{
 		Workspace *ws = &selmon->workspaces[currentworkspace()];
